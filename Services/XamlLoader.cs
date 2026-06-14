f592cbe8fb8172e0c14c7a96dffd887d68080b84
Adrian Donavann <adrian.donavann@gmail.com>
2026-06-14 02:10:11 +0200

    fix: Sentry removal, XamlLoader for .NET 10 compat, search pool, README notice

--- Services/XamlLoader.cs
+++ Services/XamlLoader.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
@@ -9,6 +10,13 @@ public static class XamlLoader
     public static void Load(this Control control)
     {
         AvaloniaXamlLoader.Load(control);
+        var fields = control.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        foreach (var field in fields)
+        {
+            var found = control.FindControl<Control>(field.Name);
+            if (found is not null && field.FieldType.IsAssignableFrom(found.GetType()))
+                field.SetValue(control, found);
+        }
     }
 
     public static void Load(this Application app)
